1c765b873b098955799e082250aca0b3d7e563db
Sqix <daisy7024@koreatech.ac.kr>
2025-03-31 11:43:01 +0900

    Fix: changed CORS settings in datasink

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -2,6 +2,8 @@
 
 export const environment = {
   production: false,
-  dataSourceApiUrl: 'http://localhost:8003',  // Base URL for Data Source backend
-  dataSinkApiUrl: 'http://localhost:8002'     // Base URL for Data Sink backend
+  // dataSourceApiUrl: 'http://localhost:8001',  // Base URL for provider datasource backend
+  // dataSinkApiUrl: 'http://localhost:8000'     // Base URL for provider datasink backend
+  dataSourceApiUrl: 'http://localhost:8003',  // Base URL for consumer datasource backend
+  dataSinkApiUrl: 'http://localhost:8002'     // Base URL for consumer datasink backend
 };
