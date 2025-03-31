45ab67430ce06f87e750bd001b2c7755e6495d2b
Sqix <daisy7024@koreatech.ac.kr>
2025-03-31 16:58:14 +0900

    Fix: rename path and resolve dockerize env issue in frontend

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -2,8 +2,6 @@
 
 export const environment = {
   production: false,
-  // dataSourceApiUrl: 'http://localhost:8001',  // Base URL for provider datasource backend
-  // dataSinkApiUrl: 'http://localhost:8000'     // Base URL for provider datasink backend
-  dataSourceApiUrl: 'http://localhost:8003',  // Base URL for consumer datasource backend
-  dataSinkApiUrl: 'http://localhost:8002'     // Base URL for consumer datasink backend
+  dataSourceApiUrl: '${DATA_SOURCE_API_URL}',  // 환경 변수로 대체될 템플릿 변수
+  dataSinkApiUrl: '${DATA_SINK_API_URL}'       // 환경 변수로 대체될 템플릿 변수
 };
